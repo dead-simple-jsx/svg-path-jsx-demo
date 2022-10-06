5c9b8eda7f3d7c53dbd5a11681c46cb921ea3aed
dead-simple <coop.lawhorn.jack@gmail.com>
2022-10-06 15:33:00 -0400

    Updates

---
+++ precache-manifest.bddf86dacfd93fa09c85efeee6b6d409.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "459694d9acef2342db64",
+    "url": "/svg-path-jsx-demo/static/css/main.98f041b0.chunk.css"
+  },
+  {
+    "revision": "459694d9acef2342db64",
+    "url": "/svg-path-jsx-demo/static/js/main.459694d9.chunk.js"
+  },
+  {
+    "revision": "7ab2393a9ad8c2b58dcd",
+    "url": "/svg-path-jsx-demo/static/js/1.7ab2393a.chunk.js"
+  },
+  {
+    "revision": "3f6a839c77fbb143877b",
+    "url": "/svg-path-jsx-demo/static/js/2.3f6a839c.chunk.js"
+  },
+  {
+    "revision": "40d1ec68a9723e7afaa7",
+    "url": "/svg-path-jsx-demo/static/js/runtime~main.40d1ec68.js"
+  },
+  {
+    "revision": "f7692adbdb35ae4ff1606beede79f17a",
+    "url": "/svg-path-jsx-demo/index.html"
+  }
+];
